0a14552848af1fef71737f260935088ff538fd75
Vanshika <duavanshika20@gmail.com>
2021-11-18 18:11:58 +0530

    1 product added

--- shop/models.py
+++ shop/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     product_id = models.AutoField
-    product_name = models.CharField(max_length=50)
-    category = models.CharField(max_length=50, default="")
+    product_name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100, default="")
     brand = models.CharField(max_length=50, default="")
     price = models.IntegerField(default=0)
-    desc = models.CharField(max_length=300)
+    desc = models.CharField(max_length=1000)
     pub_date = models.DateField()
     image = models.ImageField(upload_to='shop/images', default="")
 
